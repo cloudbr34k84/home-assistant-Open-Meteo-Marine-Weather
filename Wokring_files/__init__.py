20693df9583269eac414bb8d4e151a305755bde1
Brad <58960644+cloudbr34k84@users.noreply.github.com>
2025-07-27 15:45:59 +1000

    Delete Wokring_files directory
    duplicate directory

--- Wokring_files/__init__.py
+++
@@ -1,7 +0,0 @@
-from homeassistant.core import HomeAssistant
-
-DOMAIN = "marine_weather"
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Marine Weather integration."""
-    return True
--- Wokring_files/const.py
+++
@@ -1,2 +0,0 @@
-DOMAIN = "marine_weather"
-API_URL = "https://marine-api.open-meteo.com/v1/marine"
